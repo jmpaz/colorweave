a61d93ac2e0bc8738cf4e550e6799420d72bc02a
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-03-05 17:20:17 -0500

    Implement LDSR upscale

--- c_weave/generate.py
+++ c_weave/generate.py
@@ -1,7 +1,8 @@
-from comfy_bridge.client import Generation
+from comfy_bridge.client import Generation, upscale as comfy_upscale
 
 
 def generate_wallpaper(colors: str):
+    """Create an image using comfy_bridge's Generation class."""
     params = {
         "prompt": f"a beautiful landscape, {colors} hues",
         "prompt_negative": "poor quality",
@@ -18,4 +19,20 @@ def generate_wallpaper(colors: str):
     }
 
     gen = Generation(**params)
-    return gen.output
+    return gen.output[0]
+
+
+def upscale(input_image):
+    """Simple wrapper around comfy_bridge's upscale function."""
+    params = {
+        "steps": "100",
+        "pre_downscale": "None",
+        "post_downscale": "None",
+        "downsample_method": "Lanczos",
+    }
+    return comfy_upscale(
+        input_image,
+        params,
+        mode="ldsr",
+        workflow_path="data/workflows/upscale_ldsr.json",
+    )
